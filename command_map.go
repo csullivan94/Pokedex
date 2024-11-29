4cc5b0f427993e5669b3ab09ff0f2409e26e1e9e
csullivan94 <carasullivan94@gmail.com>
2024-11-29 22:57:03 +0000

    moved pokeapi to internal

--- command_map.go
+++ command_map.go
@@ -9,34 +9,8 @@ import (
 
 func commandMap(cfg *Config) error {
 
-	if cfg.Next == "" {
-		cfg.Next = "https://pokeapi.co/api/v2/location"
-	}
-
-	cfg.PageNum += 1
-
-	res, err := http.Get(cfg.Next)
-	if err != nil {
-		fmt.Println("error with get request: ", err)
-		return err
-	}
-
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
-
-	defer res.Body.Close()
-
-	var locations Location
-	err = json.Unmarshal(data, &locations)
-	if err != nil {
-		fmt.Printf("error unmarshaling data: %v ", err)
-		return err
-	}
-
-	for item := range locations.Results {
-		fmt.Println(locations.Results[item].Name)
+	for item := range getLocations().Results {
+		fmt.Println(getLocations().Results[item].Name)
 
 	}
 	fmt.Printf("---------------Page %d---------------\n", cfg.PageNum)
